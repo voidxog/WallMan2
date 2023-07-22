f671eac42d9b26092d589a3097c08e08268532e9
colorata <colorata.company@gmail.com>
2023-07-22 16:49:41 +0300

    Added animation on mirror screen

--- common/settings/mirror/ui/src/commonMain/kotlin/com/colorata/wallman/settings/mirror/ui/MirrorScreen.kt
+++ common/settings/mirror/ui/src/commonMain/kotlin/com/colorata/wallman/settings/mirror/ui/MirrorScreen.kt
@@ -1,11 +1,8 @@
 package com.colorata.wallman.settings.mirror.ui
 
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
@@ -14,19 +11,29 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
-import com.colorata.animateaslifestyle.material3.groupedItems
+import com.colorata.animateaslifestyle.animateVisibility
 import com.colorata.animateaslifestyle.material3.isCompact
-import com.colorata.wallman.core.data.*
+import com.colorata.animateaslifestyle.stagger.ExperimentalStaggerApi
+import com.colorata.animateaslifestyle.stagger.animateAsList
+import com.colorata.animateaslifestyle.stagger.staggerSpecOf
+import com.colorata.animateaslifestyle.stagger.toStaggerList
+import com.colorata.wallman.core.data.Destinations
+import com.colorata.wallman.core.data.MaterialNavGraphBuilder
+import com.colorata.wallman.core.data.Strings
+import com.colorata.wallman.core.data.animation
+import com.colorata.wallman.core.data.continuousComposable
 import com.colorata.wallman.core.data.module.CoreModule
-import com.colorata.wallman.core.ui.theme.spacing
+import com.colorata.wallman.core.data.rememberString
 import com.colorata.wallman.core.data.viewModel
+import com.colorata.wallman.core.ui.theme.emphasizedVerticalSlide
 import com.colorata.wallman.core.ui.theme.screenPadding
+import com.colorata.wallman.core.ui.theme.spacing
 import com.colorata.wallman.core.ui.util.LocalWindowSizeConfiguration
 import com.colorata.wallman.core.ui.util.fullLineItem
 import com.colorata.wallman.settings.mirror.api.MirrorDestination
@@ -48,7 +55,6 @@ fun MirrorScreen(modifier: Modifier = Modifier) {
     MirrorScreen(state, modifier)
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MirrorScreen(state: MirrorViewModel.MirrorScreenState, modifier: Modifier = Modifier) {
     val windowSize = LocalWindowSizeConfiguration.current
@@ -64,12 +70,19 @@ fun MirrorScreen(state: MirrorViewModel.MirrorScreenState, modifier: Modifier = 
 }
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalStaggerApi::class)
 private fun MirrorScreenLayout(
     cells: StaggeredGridCells,
     state: MirrorViewModel.MirrorScreenState,
     modifier: Modifier,
 ) {
+    val animatedMirrors = remember { state.mirrors.toStaggerList({ 0f }, visible = false) }
+    LaunchedEffect(key1 = true) {
+        animatedMirrors.animateAsList(this, spec = staggerSpecOf(itemsDelayMillis = 100) {
+            visible = true
+        })
+    }
+
     LazyVerticalStaggeredGrid(
         cells,
         modifier
@@ -79,18 +92,27 @@ private fun MirrorScreenLayout(
         verticalItemSpacing = MaterialTheme.spacing.large
     ) {
         fullLineItem {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                LargeTopAppBar(title = {
-                    Text(text = rememberString(string = Strings.mirrors))
-                })
-            }
+            LargeTopAppBar(title = {
+                Text(text = rememberString(string = Strings.mirrors))
+            })
         }
         items(
-            state.mirrors
-        ) { mirror ->
-            MirrorCard(mirror = mirror, selected = state.selectedMirror == mirror, onClick = {
-                state.onEvent(MirrorViewModel.MirrorScreenEvent.SelectMirror(mirror))
-            }, modifier = Modifier.clip(MaterialTheme.shapes.large))
+            animatedMirrors
+        ) { animated ->
+            val mirror = animated.value
+            MirrorCard(
+                mirror = mirror,
+                selected = state.selectedMirror == mirror,
+                onClick = {
+                    state.onEvent(MirrorViewModel.MirrorScreenEvent.SelectMirror(mirror))
+                },
+                modifier = Modifier
+                    .animateVisibility(
+                        animated.visible,
+                        MaterialTheme.animation.emphasizedVerticalSlide()
+                    )
+                    .clip(MaterialTheme.shapes.large)
+            )
         }
     }
 }
